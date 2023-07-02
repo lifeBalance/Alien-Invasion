961bdcc1df51b26efd3295c0760760e7ef5edf10
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-02 22:18:05 +0300

    add clock and bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,8 +8,13 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create the game resources."""
         pygame.init()
+        # The clock controls the frame rate of the game
+        self.clock = pygame.time.Clock()
+        # Set the size of the window
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        # Set the background color of the window (using RGB)
+        self.bg_color = (100, 100, 100)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -19,8 +24,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Paint the screen on each iteration, using the bg_color
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            # Make the loop run 60 times per second
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
