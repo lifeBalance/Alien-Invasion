2011f0b9230702fdca09c9e67390387019a2adf8
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-08 17:55:53 +0300

    enable Play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -200,6 +200,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks the 'Play' button."""
+        if self.play_button.btn_rect.collidepoint(mouse_pos):
+            self.game_active = True
 
     def _check_keydown_events(self, event):
         """Respond to key presses."""
--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 3.0
+        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # We'll multiply by 1 to go to the right; and by -1 to go to the left.
         self.fleet_direction = 1
